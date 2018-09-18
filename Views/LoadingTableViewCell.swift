d2cce96e5b7927b3fa0ed51e063a2dcbeb2fa0e7
andrebrilho <andre.txt@hotmail.com>
2018-09-17 21:50:03 -0300

    commit inicial GitHub

---
+++ Views/LoadingTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  LoadingTableViewCell.swift
+//  BeerApi
+//
+//  Created by André Brilho on 13/08/2018.
+//  Copyright © 2018 André Brilho. All rights reserved.
+//
+
+import UIKit
+
+public class LoadingTableViewCell: UITableViewCell {
+    @IBOutlet weak var actIndicator: UIActivityIndicatorView!
+}
